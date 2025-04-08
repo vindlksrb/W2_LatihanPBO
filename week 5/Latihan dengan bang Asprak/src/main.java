a8083f35ca6c658a7fd420afb0b968820a079993
vindlksrb <144650708+vindlksrb@users.noreply.github.com>
2025-04-08 11:35:20 +0700

    Update main.java

--- week 5/Latihan dengan bang Asprak/src/main.java
+++ week 5/Latihan dengan bang Asprak/src/main.java
@@ -1,5 +1,7 @@
-public class main {
-    public static void main(String[] args) {
+public class main 
+{
+    public static void main(String[] args) 
+    {
         mahasiswa mhs1 = new mahasiswa();
         //system.out.println("Nama: " + mhs1.nama); eror
         mhs1.tampilkanInfo();
@@ -7,7 +9,7 @@ public class main {
         System.out.println(mhs1.nama);
         System.out.println(mhs1.umur);
 
-        mahasiswa mhs2 = new mahasiswa("Budi", 20);
+        mahasiswa mhs2 = new mahasiswa("Calvin", 22);
         System.out.println("Nama: " + mhs2.nama);
         System.out.println("Umur: " + mhs2.umur);
         System.out.println(mhs2.universitas);
