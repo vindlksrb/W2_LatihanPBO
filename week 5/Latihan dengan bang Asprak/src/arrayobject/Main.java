8a1198ca596edb3ede3739dcc9589d90921c6bf8
vindlksrb <144650708+vindlksrb@users.noreply.github.com>
2025-04-08 11:39:07 +0700

    Update Main.java

--- week 5/Latihan dengan bang Asprak/src/arrayobject/Main.java
+++ week 5/Latihan dengan bang Asprak/src/arrayobject/Main.java
@@ -2,18 +2,21 @@ package arrayobject;
 
 import enkapsulasi.mahasiswa;
 
-public class Main {
+public class Main 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         //Array Object
-        mahasiswa[] student = {
-            new mahasiswa ("Ali", 20, 170),
-            new mahasiswa ("Budi", 22, 171),
-            new mahasiswa ("Caca", 19, 160),
-            new mahasiswa ("Doni", 27, 175),
+        mahasiswa[] student = 
+        {
+            new mahasiswa ("Adri", 22, 170),
+            new mahasiswa ("Calvin", 22, 171),
+            new mahasiswa ("Caca", 19, 160)
         };
-        for (mahasiswa mhs : student) {
+        for (mahasiswa mhs : student) 
+        {
             System.out.println("Nama: " + mhs.getNama() + ", Umur: " + mhs.getUmur() + ", Tinggi Badan: " + mhs.getTinggiBadan());
         }
 
@@ -21,11 +24,11 @@ public class Main {
 
         mahasiswa[] mhs = new mahasiswa[3];
         mhs[0] = new mahasiswa();
-        mhs[0].setNama("Ali");
+        mhs[0].setNama("Akbar");
         mhs[1] = new mahasiswa();
-        mhs[1].setNama("Alia");
+        mhs[1].setNama("Irfan");
         mhs[2] = new mahasiswa();
-        mhs[2].setNama("Alin");
+        mhs[2].setNama("Billy");
 
         for (mahasiswa m : mhs){
             System.out.println(m.getNama());
